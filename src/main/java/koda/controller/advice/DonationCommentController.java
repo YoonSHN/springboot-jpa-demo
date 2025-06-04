fc3fc4677565aaff8a16871f22957498d61c6040
윤승현 <tkdenddl182@gmail.com>
2025-06-05 00:31:42 +0900

    DonationControoler add

---
+++ src/main/java/koda/controller/advice/DonationCommentController.java
@@ -0,0 +1,9 @@
+package koda.controller.advice;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping()
+public class DonationCommentController {
+}
